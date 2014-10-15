26f10126216eab1baf9907856b52ef4a9abfcd8b
Ralph Cowling <acastofthousands@gmail.com>
2014-10-16 00:55:07 +0100

    boilerplate express app setup

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var redis = require("redis"),
-    client = redis.createClient();
 
 /* GET home page. */
 router.get('/', function(req, res) {
